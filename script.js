f3cb1879ca5ef99d87660aeba6ed8211fc7fdec9
Bharati Garg <bharatigargkls1999@gmail.com>
2024-10-28 15:47:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $(".slick-slider").slick({
+    slidesToShow: 3,
+    infinite: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
